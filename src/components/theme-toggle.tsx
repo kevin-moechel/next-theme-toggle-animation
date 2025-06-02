76287ac306035733bda6c0aaa35cc76e197f5058
Kevin Möchel <kevin.moechel@outlook.de>
2025-06-02 20:33:14 +0200

    Add animated theme-toggle on the index page

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Loader2, Moon, Sun } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export function ThemeToggle() {
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    // Ensure the component is mounted before rendering to avoid hydration mismatch
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return <ThemeToggleSkeleton />;
+    }
+
+    const isDark = theme === "dark";
+    const toggleTheme = async () => {
+        if (document.startViewTransition) {
+            if (!buttonRef.current) {
+                return;
+            }
+            await document.startViewTransition(() => {
+                console.log("startViewTransition");
+                setTheme(isDark ? "light" : "dark");
+            }).ready;
+
+            const { top, left } = buttonRef.current.getBoundingClientRect();
+
+            document.documentElement.animate(
+                {
+                    clipPath: [
+                        `circle(0% at ${left}px ${top}px)`,
+                        `circle(300% at ${left}px ${top}px)`,
+                    ],
+                },
+                {
+                    duration: 1000,
+                    easing: "ease-in-out",
+                    pseudoElement: "::view-transition-new(root)",
+                }
+            );
+        } else {
+            setTheme(isDark ? "light" : "dark");
+        }
+        console.log("outside transition");
+    };
+
+    return (
+        <Button
+            className="rounded-full"
+            variant="outline"
+            size="icon"
+            ref={buttonRef}
+            onClick={toggleTheme}
+        >
+            <Sun className={`${!isDark && "hidden"}`} />
+            <Moon className={`${isDark && "hidden"}`} />
+            <span className="sr-only">
+                {isDark ? "Switch to light mode" : "Switch to dark mode"}
+            </span>
+        </Button>
+    );
+}
+
+function ThemeToggleSkeleton() {
+    return (
+        <Button className="rounded-full" variant="outline" size="icon">
+            <Loader2 className="animate-spin" />
+            <span className="sr-only">Theme toggle placeholder</span>
+        </Button>
+    );
+}
